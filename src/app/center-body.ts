59c5c2fd28a8d3f3b953c467a65bd253d3935d92
gfike19 <gfike19@gmail.com>
2025-07-21 13:47:46 -0500

    changed font and background color, added text from resume regarding projects

---
+++ src/app/center-body.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-center-body',
+  imports: [],
+  templateUrl: './center-body.html',
+  styleUrl: './center-body.scss'
+})
+export class CenterBody {
+
+}
